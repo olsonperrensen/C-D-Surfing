4e488440109fa769cc4ebfd35ba9ffd43869603c
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-10-25 00:40:42 +0200

    working ads again

--- ads.php
+++ ads.php
@@ -1,6 +1,7 @@
 <?php include_once 'includes/header.php'; ?>
 <?php if (!empty($email)) : ?>
     <?php include_once "pdo.php" ?>
+    <?php include_once "models/Pet.php" ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <div class="p-3 mb-2 bg-dark text-white">
         <p class="lead text-white text-center">
@@ -11,61 +12,40 @@
         <div class="container">
             <div class="row">
                 <?php
-                $sql = "SELECT * FROM ads;";
+                $sql = "SELECT * FROM pet_details where on_adoption = 1;";
                 $stmt = $pdo->prepare($sql);
                 $stmt->execute();
                 $row = $stmt->fetch(PDO::FETCH_ASSOC);
-                $i = 0;
                 while ($row) {
-                    $i++;
-                    $ownerID = $row['ownerID'];
-                    $breedID = $row['breedID'];
-                    $name = $row['name'];
-                    $gender = $row['gender'];
-                    $age = $row['age'];
-                    $size = $row['size'];
-                    $color = $row['color'];
-                    $story = $row['story'];
-                    $diet = $row['diet'];
-                    $isDog = $row['isDog'];
+                    $pet = new Pet($row);
                     echo <<<AD
                     <div class="col-md-6 col-lg-4">
                     <div class="card my-3">
                     <div class="card-thumbnail">
-                    <img src="./assets/img/generic_$isDog.png" width="200" class="img-fluid mx-auto d-block" alt="thumbnail">
+                    <img src="./assets/img/breed_details.jpg" width="200" class="img-fluid mx-auto d-block" alt="thumbnail">
                     </div>
                     <div class="card-body">
                     <div class="row">
                     <div class="col-4">
-                    <div class="list-group" id="list-tab$i" role="tablist">
-                    <a class="list-group-item list-group-item-action active" id="list-home-list$i" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">Home</a>
-                    <a class="list-group-item list-group-item-action" id="list-profile-list$i" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Profile</a>
-                    <a class="list-group-item list-group-item-action" id="list-messages-list$i" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Messages</a>
-                    <a class="list-group-item list-group-item-action" id="list-settings-list$i" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Settings</a>
+                    <div class="list-group" id="list-tab$pet->pet_id" role="tablist">
+                    <a class="list-group-item list-group-item-action active" id="list-home-list$pet->pet_id" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">Profile</a>
+                    <a class="list-group-item list-group-item-action" id="list-profile-list$pet->pet_id" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Story</a>
+                    <a class="list-group-item list-group-item-action" id="list-messages-list$pet->pet_id" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Diet</a>
+                    <a class="list-group-item list-group-item-action" id="list-settings-list$pet->pet_id" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Location</a>
                     </div>
                     </div>
                     <div class="col-8">
-                    <div class="bg-light tab-content" id="nav-tabContent$i">
-                    <div class="tab-pane active" id="list-home$i" role="tabpanel" aria-labelledby="list-home-list">
+                    <div class="bg-light tab-content" id="nav-tabContent$pet->pet_id">
+                    <div class="tab-pane active" id="list-home$pet->pet_id" role="tabpanel" aria-labelledby="list-home-list">
                     AD;
-                    if ($isDog) {
-                        $sql2 = "SELECT name from dogs where id = $breedID";
-                        $stmt2 = $pdo->prepare($sql2);
-                        $stmt2->execute();
-                        $row2 = $stmt2->fetch(PDO::FETCH_ASSOC);
-                        echo $row2['name'];
-                    } else {
-                        $sql3 = "SELECT name from cats where id = $breedID";
-                        $stmt3 = $pdo->prepare($sql3);
-                        $stmt3->execute();
-                        $row3 = $stmt3->fetch(PDO::FETCH_ASSOC);
-                        echo $row3['name'];
+                    foreach ($pet as $key => $value) {
+                        echo "$key:$value,";
                     }
                     echo <<<ADD
-                     $breedID $name $gender $age $size $color</div>
-                    <div class="tab-pane" id="list-profile$i" role="tabpanel" aria-labelledby="list-profile-list">$story</div>
-                    <div class="tab-pane" id="list-messages$i" role="tabpanel" aria-labelledby="list-messages-list">$diet</div>
-                    <div class="tab-pane" id="list-settings$i" role="tabpanel" aria-labelledby="list-settings-list">$ownerID</div>
+                    </div>
+                    <div class="tab-pane" id="list-profile$pet->pet_id" role="tabpanel" aria-labelledby="list-profile-list">$pet->story</div>
+                    <div class="tab-pane" id="list-messages$pet->pet_id" role="tabpanel" aria-labelledby="list-messages-list">$pet->diet</div>
+                    <div class="tab-pane" id="list-settings$pet->pet_id" role="tabpanel" aria-labelledby="list-settings-list">$pet->owner_id</div>
                     </div>
                     </div>
                     </div>
@@ -74,51 +54,51 @@
                     </div>
                     <script>
                     $(document).ready(function() {
-                    $("#list-home-list$i").click(function() {
-                    $("#list-home$i").toggle();
-                    $("#list-profile$i").hide()
-                    $("#list-messages$i").hide();
-                    $("#list-settings$i").hide();
+                    $("#list-home-list$pet->pet_id").click(function() {
+                    $("#list-home$pet->pet_id").toggle();
+                    $("#list-profile$pet->pet_id").hide()
+                    $("#list-messages$pet->pet_id").hide();
+                    $("#list-settings$pet->pet_id").hide();
     
-                    $("#list-home-list$i").addClass('active');
-                    $("#list-profile-list$i").removeClass('active');
-                    $("#list-messages-list$i").removeClass('active');
-                    $("#list-settings-list$i").removeClass('active');
+                    $("#list-home-list$pet->pet_id").addClass('active');
+                    $("#list-profile-list$pet->pet_id").removeClass('active');
+                    $("#list-messages-list$pet->pet_id").removeClass('active');
+                    $("#list-settings-list$pet->pet_id").removeClass('active');
                     });
-                    $("#list-profile-list$i").click(function() {
-                    $("#list-profile$i").toggle();
-                    $("#list-home$i").hide()
-                    $("#list-messages$i").hide();
-                    $("#list-settings$i").hide();
+                    $("#list-profile-list$pet->pet_id").click(function() {
+                    $("#list-profile$pet->pet_id").toggle();
+                    $("#list-home$pet->pet_id").hide()
+                    $("#list-messages$pet->pet_id").hide();
+                    $("#list-settings$pet->pet_id").hide();
     
-                    $("#list-profile-list$i").addClass('active');
-                    $("#list-home-list$i").removeClass('active');
-                    $("#list-messages-list$i").removeClass('active');
-                    $("#list-settings-list$i").removeClass('active');
+                    $("#list-profile-list$pet->pet_id").addClass('active');
+                    $("#list-home-list$pet->pet_id").removeClass('active');
+                    $("#list-messages-list$pet->pet_id").removeClass('active');
+                    $("#list-settings-list$pet->pet_id").removeClass('active');
                     });
-                    $("#list-messages-list$i").click(function() {
-                    $("#list-messages$i").toggle();
-                    $("#list-profile$i").hide()
-                    $("#list-home$i").hide();
-                    $("#list-settings$i").hide();
+                    $("#list-messages-list$pet->pet_id").click(function() {
+                    $("#list-messages$pet->pet_id").toggle();
+                    $("#list-profile$pet->pet_id").hide()
+                    $("#list-home$pet->pet_id").hide();
+                    $("#list-settings$pet->pet_id").hide();
     
     
-                    $("#list-messages-list$i").addClass('active');
-                    $("#list-home-list$i").removeClass('active');
-                    $("#list-profile-list$i").removeClass('active');
-                    $("#list-settings-list$i").removeClass('active');
+                    $("#list-messages-list$pet->pet_id").addClass('active');
+                    $("#list-home-list$pet->pet_id").removeClass('active');
+                    $("#list-profile-list$pet->pet_id").removeClass('active');
+                    $("#list-settings-list$pet->pet_id").removeClass('active');
                     });
-                    $("#list-settings-list$i").click(function() {
-                    $("#list-settings$i").toggle();
-                    $("#list-profile$i").hide()
-                    $("#list-messages$i").hide();
-                    $("#list-home$i").hide();
+                    $("#list-settings-list$pet->pet_id").click(function() {
+                    $("#list-settings$pet->pet_id").toggle();
+                    $("#list-profile$pet->pet_id").hide()
+                    $("#list-messages$pet->pet_id").hide();
+                    $("#list-home$pet->pet_id").hide();
     
     
-                    $("#list-settings-list$i").addClass('active');
-                    $("#list-home-list$i").removeClass('active');
-                    $("#list-profile-list$i").removeClass('active');
-                    $("#list-messages-list$i").removeClass('active');
+                    $("#list-settings-list$pet->pet_id").addClass('active');
+                    $("#list-home-list$pet->pet_id").removeClass('active');
+                    $("#list-profile-list$pet->pet_id").removeClass('active');
+                    $("#list-messages-list$pet->pet_id").removeClass('active');
                     });
                     });
                     </script>
---
+++ models/Pet.php
@@ -0,0 +1,21 @@
+<?php
+class Pet
+{
+    public function __construct(array $properties = array())
+    {
+        foreach ($properties as $key => $value) {
+            $this->{$key} = $value;
+        }
+    }
+    public $pet_id;
+    public $owner_id;
+    public $breed_id;
+    public $name;
+    public $gender;
+    public $age;
+    public $size;
+    public $color;
+    public $story;
+    public $diet;
+    public $on_adoption;
+}
