8d990d0e36160eaa90359f378e8c00ade12d792e
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-11-15 18:01:36 +0100

    Fix to 404 for ppl w/o pets

--- user_pets.php
+++ user_pets.php
@@ -14,57 +14,61 @@
                 $stmt = $pdo->prepare($sql);
                 $stmt->execute(array(':u' => $email));
                 $row = $stmt->fetch(PDO::FETCH_ASSOC);
-                foreach ($row as $key => $value) {
-                    if ($key == 'healthcare_id') continue;
-                    echo <<<Q
-            <td>$key</td>
-            Q;
-                }
-                echo "</tr></thead><tbody>";
-                while ($row) {
-                    $pet = new Pet($row);
-                    echo '<tr class="text-';
-                    switch (strlen($pet->name)) {
-                        case 3:
-                            echo 'gray';
-                            break;
-                        case 4:
-                            echo 'secondary';
-                            break;
-                        case 5:
-                            echo 'success';
-                            break;
-                        case 6:
-                            echo 'danger';
-                            break;
-                        case 7:
-                            echo 'warning';
-                            break;
-                        case 8:
-                            echo 'info';
-                            break;
-                        case 9:
-                            echo 'light';
-                            break;
-                        default:
-                            echo 'primary';
-                            break;
-                    }
-                    echo '">';
+                if (!empty($row)) {
                     foreach ($row as $key => $value) {
                         if ($key == 'healthcare_id') continue;
-                        else if ($key == 'healthcare_name') {
-                            echo <<<H
-                            <td><a href="healthcare_details.php?h=$pet->healthcare_id">$value</a></td>
-                        H;
-                            continue;
-                        }
-                        echo <<<T
-                            <td>$value</td>
-                        T;
+                        echo <<<Q
+                <td>$key</td>
+                Q;
                     }
-                    echo '</tr>';
-                    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                    echo "</tr></thead><tbody>";
+                    while ($row) {
+                        $pet = new Pet($row);
+                        echo '<tr class="text-';
+                        switch (strlen($pet->name)) {
+                            case 3:
+                                echo 'gray';
+                                break;
+                            case 4:
+                                echo 'secondary';
+                                break;
+                            case 5:
+                                echo 'success';
+                                break;
+                            case 6:
+                                echo 'danger';
+                                break;
+                            case 7:
+                                echo 'warning';
+                                break;
+                            case 8:
+                                echo 'info';
+                                break;
+                            case 9:
+                                echo 'light';
+                                break;
+                            default:
+                                echo 'primary';
+                                break;
+                        }
+                        echo '">';
+                        foreach ($row as $key => $value) {
+                            if ($key == 'healthcare_id') continue;
+                            else if ($key == 'healthcare_name') {
+                                echo <<<H
+                                <td><a href="healthcare_details.php?h=$pet->healthcare_id">$value</a></td>
+                            H;
+                                continue;
+                            }
+                            echo <<<T
+                                <td>$value</td>
+                            T;
+                        }
+                        echo '</tr>';
+                        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                    }
+                } else {
+                    header('Location: 404.php');
                 }
                 ?>
     </table>
