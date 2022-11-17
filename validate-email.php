c02d914cfcd60c906662260077618eaa5d665c16
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-11-17 16:22:25 +0100

    missing files

---
+++ validate-email.php
@@ -0,0 +1,15 @@
+<?php include_once 'pdo.php'; ?>
+<?php
+if (!empty($_GET['email'])) {
+    $new_user_email = htmlspecialchars($_GET['email'], ENT_QUOTES);
+    $sql = "SELECT * FROM users WHERE email = :uem;";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(':uem' => $new_user_email));
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    header("Content-Type: application/json");
+    if (!empty($row)) {
+        echo json_encode(["vrij" => false]);
+    } else {
+        echo json_encode(["vrij" => true]);
+    }
+}
