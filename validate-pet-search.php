baeff2bee35425af4f2d12fabf37ea06d16de44e
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-11-19 16:53:30 +0100

    API filter pet by breed ok

---
+++ validate-pet-search.php
@@ -0,0 +1,28 @@
+<?php include_once 'pdo.php'; ?>
+<?php
+session_start();
+if (
+    !empty($_SESSION['email']) && !empty($_GET['q'])
+    && is_numeric($_GET['q'])
+) {
+    $filtered_pets_by_breed = array();
+    $breed_id = htmlspecialchars($_GET['q'], ENT_QUOTES);
+
+    $sql = "SELECT p.pet_id,b.name as bname, b.isFeline, gender, age,p.name,story,diet,u.zipcode,datediff(now(),a.advertised_date) as days FROM pet_details p
+                    join ads a on p.pet_id=a.pet_id
+                    join users u on p.owner_id =u.user_id
+                    join breeds b on p.breed_id= b.breed_id
+                    where p.breed_id = :b
+                    order by days;";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(":b" => $breed_id));
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    while ($row) {
+        array_push($filtered_pets_by_breed, $row);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    header("Content-Type: application/json");
+    echo json_encode($filtered_pets_by_breed);
+} else {
+    header('Location: index.php');
+}
