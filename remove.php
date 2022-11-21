6febf31118d0bd7f21cc81bbf557e86bff9e013a
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-11-22 00:01:20 +0100

    remove users from admin panel OK

--- remove.php
+++ remove.php
@@ -8,5 +8,20 @@ if (!empty($_GET['pet_id']) && is_numeric($_GET['pet_id'])) {
         $stmt->execute(array(':pid' => $pid));
     } catch (PDOException $e) {
     }
+    header("Location: order.php");
 }
-header("Location: order.php");
+if (!empty($_GET['user_id']) && is_numeric($_GET['user_id'])) {
+    session_start();
+    $isAdmin = $_SESSION['isAdmin'];
+    if ($isAdmin) {
+        try {
+            $user_id_d = htmlspecialchars($_GET['user_id'], ENT_QUOTES);
+            $sql = "DELETE FROM USERS WHERE user_id = :uid";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(array(':uid' => $user_id_d));
+        } catch (PDOException $e) {
+        }
+        header("Location: manage_users.php");
+    }
+}
+header("Location: account.php");
