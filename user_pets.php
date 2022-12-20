ce4ff68d986988821b5749badd2a2cbe9298a8c6
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-12-21 00:25:11 +0100

    (buggy) user chart

--- user_pets.php
+++ user_pets.php
@@ -31,7 +31,8 @@
                 $total_paid = 0.00;
                 $sql = "SELECT p.name, b.name as breed,
                 gender, age, size, color, diet, h.healthcare_id, h.healthcare_name, register_date,
-                h.price from pet_details p
+                h.price, o.date_created as purchased_on from pet_details p
+                join orders o on o.user_id = p.owner_id
                 join breeds b on b.breed_id = p.breed_id
                 join users u on p.owner_id = u.user_id
                 join healthcare h on h.healthcare_id = p.healthcare_id
