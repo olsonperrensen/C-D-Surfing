9eeb03bfe582924ec3e046256845a91a01c30e89
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-12-24 22:15:10 +0100

    docker-option pdo.pjp

--- pdo.php
+++ pdo.php
@@ -1,6 +1,16 @@
 <?php
+
+// USE this if you run it locally on your OS server
 $pdo = new PDO(
     'mysql:host=localhost;port=3306;dbname=pets',
+    'Webuser',
+    "Lab2021",
+    array(PDO::MYSQL_ATTR_FOUND_ROWS => true)
+);
+
+// USE this while running on Docker
+$pdo = new PDO(
+    'mysql:host=mysql;port=3306;dbname=pets',
     'Webuser',
     "Lab2021",
     array(PDO::MYSQL_ATTR_FOUND_ROWS => true)
