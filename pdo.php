8d149ea4d64472cca10a53be05f50bc9397ffa23
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-12-24 22:24:21 +0100

    docker NOT default connection

--- pdo.php
+++ pdo.php
@@ -1,16 +1,19 @@
 <?php
+// ~~~~~~~~ IMPORTANT ! ~~~~~~~~
 
-// USE this if you run it locally on your OS server
+
+// If you are running it locally and directly via (X)AMP: 
+//      *USE* host=localhost 
+
+
+
+// Else, if you are running Docker: 
+//      *USE* host=mysql
+
+
+// Configured for local deployment as default
 $pdo = new PDO(
     'mysql:host=localhost;port=3306;dbname=pets',
-    'Webuser',
-    "Lab2021",
-    array(PDO::MYSQL_ATTR_FOUND_ROWS => true)
-);
-
-// USE this while running on Docker
-$pdo = new PDO(
-    'mysql:host=mysql;port=3306;dbname=pets',
     'Webuser',
     "Lab2021",
     array(PDO::MYSQL_ATTR_FOUND_ROWS => true)
