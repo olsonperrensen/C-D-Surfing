333b687bc3c8819bff8179526fa1c355d7f351f7
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-12-26 20:57:49 +0100

    fix html valid

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php include_once 'includes/header.php'; ?>
 <section class="page-section cta">
-<div class="container">
+    <div class="container">
         <div class="row">
             <div class="col-xl-9 mx-auto">
                 <div class="cta-inner bg-faded text-center rounded">
