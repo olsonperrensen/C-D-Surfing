c61b04de5420a461f9a40f87650980ce72077a4e
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-12-26 17:10:20 +0100

    Master branch made compatible for both deployment and developemtn environments

--- pdo.php
+++ pdo.php
@@ -10,12 +10,20 @@
 // Else, if you are running Docker: 
 //      *USE* host=mysql
 
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL")) ?? '';
+$cleardb_server = $cleardb_url["host"] ?? 'localhost';
+$cleardb_username = $cleardb_url["user"] ?? 'Webuser';
+$cleardb_password = $cleardb_url["pass"] ?? 'Lab2021';
+$cleardb_db = substr($cleardb_url["path"], 1) ?? 'pets';
+$active_group = 'default';
+$query_builder = TRUE;
 
 // Configured for local deployment as default
 $pdo = new PDO(
-    'mysql:host=localhost;port=3306;dbname=pets',
-    'Webuser',
-    "Lab2021",
+    "mysql:host=$cleardb_server;port=3306;dbname=$cleardb_db",
+    "$cleardb_username",
+    "$cleardb_password",
     array(PDO::MYSQL_ATTR_FOUND_ROWS => true)
 );
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
