01317529be481c195280abd47c7eb37fe4b2ef6f
Max Imilian o <80784184+olsonperrensen@users.noreply.github.com>
2025-09-23 21:29:35 +0200

    Update pdo.php
    Dockerized as default

--- pdo.php
+++ pdo.php
@@ -12,7 +12,7 @@
 
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL")) ?? '';
-$cleardb_server = $cleardb_url["host"] ?? 'localhost';
+$cleardb_server = $cleardb_url["host"] ?? 'mysql';
 $cleardb_username = $cleardb_url["user"] ?? 'Webuser';
 $cleardb_password = $cleardb_url["pass"] ?? 'Lab2021';
 $cleardb_db = substr($cleardb_url["path"], 1) != '' && substr($cleardb_url["path"], 1) != NULL ? substr($cleardb_url["path"], 1) : 'pets';
